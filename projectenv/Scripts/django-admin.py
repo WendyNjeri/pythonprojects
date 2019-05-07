3f79255008418b79ef7d5244eb46aeb97084e550
WendyNjeri <wendynjerim@gmail.com>
2019-05-07 10:19:21 +0300

    first commit

---
+++ projectenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\wendy\Documents\python projects\projectenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
